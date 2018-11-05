906bfd350221c2295c555b4d6b766d732617664e
Srinivas <Srinivas@DESKTOP-JUR5U8I>
2018-11-05 16:33:14 +0530

    FCommit

---
+++ abcJavaProject/src/abcJavaProject/AbcJava.java
@@ -0,0 +1,14 @@
+package abcJavaProject;
+
+public class AbcJava {
+
+	public AbcJava() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+	}
+
+}
